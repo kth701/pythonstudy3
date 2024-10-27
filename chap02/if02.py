c1c2353557b5a777d58721b9caece02e85bc1bd4
kth701 <kth701_nate.com>
2024-10-27 14:22:09 +0900

    - while 01

--- chap02/if02.py
+++ chap02/if02.py
@@ -9,5 +9,24 @@
 #273은 5로 나누어 떨어지는 숫자가 아닙니다.
 
 print("-- 2,3,4,5나누어 떨어지는 숫자인지 판별하기")
+inputNum = int(input('정수를 입력해주세요: '))
 
+if inputNum%2 == 0:
+    print(f"{inputNum}은 2로 나누어 떨어지는 숫자 입니다.")
+else:
+    print(f"{inputNum}은 2로 나누어 떨어지는 숫자가 입니다.")
 
+if inputNum%3 == 0:
+    print(f"{inputNum}은 3로 나누어 떨어지는 숫자 입니다.")
+else:
+    print(f"{inputNum}은 3로 나누어 떨어지는 숫자가 입니다.")
+
+if inputNum%4 == 0:
+    print(f"{inputNum}은 4로 나누어 떨어지는 숫자 입니다.")
+else:
+    print(f"{inputNum}은 4로 나누어 떨어지는 숫자가 입니다.")
+
+if inputNum%5 == 0:
+    print(f"{inputNum}은 5로 나누어 떨어지는 숫자 입니다.")
+else:
+    print(f"{inputNum}은 5로 나누어 떨어지는 숫자가 입니다.")    
